4a37a7200bd56f256039cfff808ec5116e2266f0
amgad1982 <amgad.awad@gmail.com>
2019-02-19 02:13:10 +0300

    initial commit

---
+++ SecureApi/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Swashbuckle.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(SecureApi.Startup))]
+
+namespace SecureApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+            config.EnableSwagger(c => c.SingleApiVersion("v1", "A title for your API"))
+                      .EnableSwaggerUi();
+        }
+    }
+}
